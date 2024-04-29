ed1bc5975d1f5f6ee7e29f9d376775211a21e1a2
Juozas Gaigalas <juozasgaigalas@gmail.com>
2024-04-29 23:50:53 +0300

    build bugging out

--- src/main/java/com/juozasg/keylab/KeylabPlusPlusExtension.java
+++ src/main/java/com/juozasg/keylab/KeylabPlusPlusExtension.java
@@ -1,79 +1,79 @@
-package com.juozasg.keylab;
+// package com.juozasg.keylab;
 
-import com.bitwig.extension.api.util.midi.ShortMidiMessage;
-import com.bitwig.extension.callback.ShortMidiMessageReceivedCallback;
-import com.bitwig.extension.controller.api.ControllerHost;
-import com.bitwig.extension.controller.api.Transport;
-import com.bitwig.extension.controller.ControllerExtension;
+// import com.bitwig.extension.api.util.midi.ShortMidiMessage;
+// import com.bitwig.extension.callback.ShortMidiMessageReceivedCallback;
+// import com.bitwig.extension.controller.api.ControllerHost;
+// import com.bitwig.extension.controller.api.Transport;
+// import com.bitwig.extension.controller.ControllerExtension;
 
-public class KeylabPlusPlusExtension extends ControllerExtension
-{
-   protected KeylabPlusPlusExtension(final KeylabPlusPlusExtensionDefinition definition, final ControllerHost host)
-   {
-      super(definition, host);
-   }
+// public class KeylabPlusPlusExtension extends ControllerExtension
+// {
+//    protected KeylabPlusPlusExtension(final KeylabPlusPlusExtensionDefinition definition, final ControllerHost host)
+//    {
+//       super(definition, host);
+//    }
 
-   @Override
-   public void init()
-   {
-      final ControllerHost host = getHost();
+//    @Override
+//    public void init()
+//    {
+//       final ControllerHost host = getHost();
 
-      mTransport = host.createTransport();
-      host.getMidiInPort(0).setMidiCallback((ShortMidiMessageReceivedCallback)msg -> onMidi0(msg));
-      host.getMidiInPort(0).setSysexCallback((String data) -> onSysex0(data));
-      host.getMidiInPort(1).setMidiCallback((ShortMidiMessageReceivedCallback)msg -> onMidi1(msg));
-      host.getMidiInPort(1).setSysexCallback((String data) -> onSysex1(data));
+//       mTransport = host.createTransport();
+//       host.getMidiInPort(0).setMidiCallback((ShortMidiMessageReceivedCallback)msg -> onMidi0(msg));
+//       host.getMidiInPort(0).setSysexCallback((String data) -> onSysex0(data));
+//       host.getMidiInPort(1).setMidiCallback((ShortMidiMessageReceivedCallback)msg -> onMidi1(msg));
+//       host.getMidiInPort(1).setSysexCallback((String data) -> onSysex1(data));
 
-      // TODO: Perform your driver initialization here.
-      // For now just show a popup notification for verification that it is running.
-      host.showPopupNotification("keylab-plusplus Initialized");
-   }
+//       // TODO: Perform your driver initialization here.
+//       // For now just show a popup notification for verification that it is running.
+//       host.showPopupNotification("keylab-plusplus Initialized");
+//    }
 
-   @Override
-   public void exit()
-   {
-      // TODO: Perform any cleanup once the driver exits
-      // For now just show a popup notification for verification that it is no longer running.
-      getHost().showPopupNotification("keylab-plusplus Exited");
-   }
+//    @Override
+//    public void exit()
+//    {
+//       // TODO: Perform any cleanup once the driver exits
+//       // For now just show a popup notification for verification that it is no longer running.
+//       getHost().showPopupNotification("keylab-plusplus Exited");
+//    }
 
-   @Override
-   public void flush()
-   {
-      // TODO Send any updates you need here.
-   }
+//    @Override
+//    public void flush()
+//    {
+//       // TODO Send any updates you need here.
+//    }
 
-   /** Called when we receive short MIDI message on port 0. */
-   private void onMidi0(ShortMidiMessage msg)
-   {
-      // TODO: Implement your MIDI input handling code here.
-   }
+//    /** Called when we receive short MIDI message on port 0. */
+//    private void onMidi0(ShortMidiMessage msg)
+//    {
+//       // TODO: Implement your MIDI input handling code here.
+//    }
 
-   /** Called when we receive sysex MIDI message on port 0. */
-   private void onSysex0(final String data)
-   {
-      // MMC Transport Controls:
-      if (data.equals("f07f7f0605f7"))
-            mTransport.rewind();
-      else if (data.equals("f07f7f0604f7"))
-            mTransport.fastForward();
-      else if (data.equals("f07f7f0601f7"))
-            mTransport.stop();
-      else if (data.equals("f07f7f0602f7"))
-            mTransport.play();
-      else if (data.equals("f07f7f0606f7"))
-            mTransport.record();
-   }
-   /** Called when we receive short MIDI message on port 1. */
-   private void onMidi1(ShortMidiMessage msg)
-   {
-      // TODO: Implement your MIDI input handling code here.
-   }
+//    /** Called when we receive sysex MIDI message on port 0. */
+//    private void onSysex0(final String data)
+//    {
+//       // MMC Transport Controls:
+//       if (data.equals("f07f7f0605f7"))
+//             mTransport.rewind();
+//       else if (data.equals("f07f7f0604f7"))
+//             mTransport.fastForward();
+//       else if (data.equals("f07f7f0601f7"))
+//             mTransport.stop();
+//       else if (data.equals("f07f7f0602f7"))
+//             mTransport.play();
+//       else if (data.equals("f07f7f0606f7"))
+//             mTransport.record();
+//    }
+//    /** Called when we receive short MIDI message on port 1. */
+//    private void onMidi1(ShortMidiMessage msg)
+//    {
+//       // TODO: Implement your MIDI input handling code here.
+//    }
 
-   /** Called when we receive sysex MIDI message on port 1. */
-   private void onSysex1(final String data)
-   {
-   }
+//    /** Called when we receive sysex MIDI message on port 1. */
+//    private void onSysex1(final String data)
+//    {
+//    }
 
-   private Transport mTransport;
-}
+//    private Transport mTransport;
+// }
